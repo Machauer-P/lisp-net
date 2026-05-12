b958708b147190fee8923e85383b2b25af092ef5
Paul <pmachaue@stud.hs-offenburg.de>
2026-05-12 22:21:48 +0200

    correct loading of utils

--- deployment/keras_to_tf_js.py
+++ deployment/keras_to_tf_js.py
@@ -17,6 +17,10 @@ import os
 import shutil
 import tempfile
 
+_PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if _PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, _PROJECT_ROOT)
+
 from utils.model_loading import load_keras_model
 
 
